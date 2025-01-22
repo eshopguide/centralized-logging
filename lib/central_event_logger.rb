9e1d82af21df517a6970f03aa247eb4441e3fe44
DaveEshopGuide <dave@eshop-guide.de>
2025-01-22 11:08:35 +0100

    Enhance event logging by skipping if API base URL is not configured and bump version to 0.1.7

--- lib/central_event_logger.rb
+++ lib/central_event_logger.rb
@@ -5,8 +5,6 @@ require "active_job"
 require_relative "central_event_logger/configuration"
 require "central_event_logger/railtie" if defined?(Rails)
 
-
-
 # CentralEventLogger is a library for logging events to a central database.
 module CentralEventLogger
   class << self
@@ -18,7 +16,11 @@ module CentralEventLogger
     yield(configuration)
   end
 
-  def self.log_event(event_name:, event_type:, customer_myshopify_domain:, customer_info: {}, event_value: nil, payload: {}, timestamp: Time.now, app_name: nil, external_id: nil)
+  def self.log_event(event_name:, event_type:, customer_myshopify_domain:, customer_info: {},
+                     event_value: nil, payload: {}, timestamp: Time.now, app_name: nil, external_id: nil)
+    # Skip logging if API base URL is not configured
+    return if ENV["CENTRAL_EVENT_LOGGER_API_BASE_URL"].nil?
+
     # Validate required parameters
     raise ArgumentError, "event_name is required" unless event_name
     raise ArgumentError, "event_type is required" unless event_type
